97f47299bcf152572eaab1f0e64fdc629965992d
Gustav Burchardt <gustavburchardt@gmail.com>
2020-03-27 00:23:02 +0100

    Make flame white

--- modules/game1585264969013.js
+++ modules/game1585264969013.js
@@ -39,7 +39,7 @@ window.startGame = function startGame (initialLetters) {
   }
 
   const playerImg = new window.Image()
-  playerImg.src = '/modules/rocket1585264320136.png'
+  playerImg.src = '/modules/rocket1585264969013.png'
 
   let score = 0
 
@@ -287,6 +287,7 @@ window.startGame = function startGame (initialLetters) {
     ctx.lineTo(player.w * 0.08, player.h * 0.35)
     const vel = keys.ArrowUp ? 0.3 : 0
     ctx.lineTo(0, player.h * (0.35 + vel))
+    ctx.fillStyle = 'white'
     ctx.fill()
     ctx.restore()
 
