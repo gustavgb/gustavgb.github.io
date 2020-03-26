4a15ccf0d53a10b4521e0c7099bd64526f3853aa
Gustav Burchardt <gustavburchardt@gmail.com>
2020-03-26 22:36:15 +0100

    Implement collision

--- modules/game1585258547178.js
+++ modules/game1585258547178.js
@@ -30,25 +30,27 @@ window.startGame = function startGame (initialLetters) {
     h: 100,
     vx: 0,
     vy: 0,
-    angle: 0,
+    angle: -0.5 * Math.PI,
     lastShootTime: 0
   }
 
   const playerImg = new window.Image()
-  playerImg.src = '/modules/rocket1585256898805.png'
+  playerImg.src = '/modules/rocket1585258547178.png'
 
   const bullets = []
 
   function shoot () {
     bullets.push({
-      x: player.x + Math.cos(player.angle - 0.5 * Math.PI) * 50,
-      y: player.y + Math.sin(player.angle - 0.5 * Math.PI) * 50,
-      vx: Math.cos(player.angle - 0.5 * Math.PI) * 10,
-      vy: Math.sin(player.angle - 0.5 * Math.PI) * 10
+      x: player.x + Math.cos(player.angle) * 50,
+      y: player.y + Math.sin(player.angle) * 50,
+      vx: Math.cos(player.angle) * 10,
+      vy: Math.sin(player.angle) * 10,
+      angle: player.angle
     })
   }
 
   const letters = []
+  window.letters = letters
 
   function addLetter (letter, x, y, w, h) {
     const velocity = h / 1000
@@ -61,7 +63,7 @@ window.startGame = function startGame (initialLetters) {
       h,
       vx: Math.cos(direction) * velocity,
       vy: Math.sin(direction) * velocity,
-      angle: 0,
+      angle: -0.5 * Math.PI,
       spin: (Math.random() * 0.01 - 0.005) * velocity / 3
     })
   }
@@ -74,6 +76,8 @@ window.startGame = function startGame (initialLetters) {
     obj.h
   ))
 
+  console.log(letters)
+
   function loop () {
     setTimeout(loop, 1000 / 60)
 
@@ -84,8 +88,8 @@ window.startGame = function startGame (initialLetters) {
       player.angle += 0.1
     }
     if (keys.ArrowUp && player.vx * player.vy < 100) {
-      player.vx += Math.cos(player.angle - 0.5 * Math.PI) * 0.05
-      player.vy += Math.sin(player.angle - 0.5 * Math.PI) * 0.05
+      player.vx += Math.cos(player.angle) * 0.05
+      player.vy += Math.sin(player.angle) * 0.05
     }
     if (keys[' '] && Date.now() - player.lastShootTime > 500) {
       player.lastShootTime = Date.now()
@@ -113,17 +117,38 @@ window.startGame = function startGame (initialLetters) {
       letter.angle += letter.spin
     })
 
-    bullets.forEach(bullet => {
+    for (let i = bullets.length - 1; i >= 0; i--) {
+      const bullet = bullets[i]
       bullet.x += bullet.vx
       bullet.y += bullet.vy
-    })
+
+      for (let j = letters.length - 1; j >= 0; j--) {
+        const letter = letters[j]
+        const dx = letter.x - bullet.x
+        const dy = letter.y - bullet.y
+        const angle = Math.atan2(dy, dx)
+        const len = Math.sqrt(dx * dx + dy * dy)
+        const bulletX = letter.x + Math.cos(0.5 * Math.PI - angle + letter.angle) * len
+        const bulletY = letter.y + Math.sin(0.5 * Math.PI - angle + letter.angle) * len
+
+        if (
+          bulletX <= letter.x + letter.w * 0.45 &&
+          letter.x - letter.w * 0.45 <= bulletX &&
+          bulletY <= letter.y + letter.h * 0.45 &&
+          letter.y - letter.h * 0.45 <= bulletY
+        ) {
+          bullets.splice(i, 1)
+          letters.splice(j, 1)
+        }
+      }
+    }
 
     ctx.clearRect(0, 0, canvas.width, canvas.height)
 
     letters.forEach(letter => {
       ctx.save()
       ctx.translate(letter.x, letter.y)
-      ctx.rotate(letter.angle)
+      ctx.rotate(letter.angle + 0.5 * Math.PI)
       ctx.textBaseline = 'middle'
       ctx.textAlign = 'center'
       ctx.fillStyle = 'black'
@@ -135,15 +160,15 @@ window.startGame = function startGame (initialLetters) {
     bullets.forEach(bullet => {
       ctx.save()
       ctx.translate(bullet.x, bullet.y)
-      ctx.rotate(bullet.angle)
+      ctx.rotate(bullet.angle + 0.5 * Math.PI)
       ctx.fillStyle = 'black'
-      ctx.fillRect(-6, -3, 12, 6)
+      ctx.fillRect(-3, -6, 6, 12)
       ctx.restore()
     })
 
     ctx.save()
     ctx.translate(player.x, player.y)
-    ctx.rotate(player.angle)
+    ctx.rotate(player.angle + 0.5 * Math.PI)
     ctx.drawImage(playerImg, -player.w / 2, -player.w / 2, player.w, player.h)
     ctx.restore()
   }
